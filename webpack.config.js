34e83221714ccfa3321ef56f9b20b93b9ef6d6d5
刘欢 <sias_xg_217@126.com>
2018-12-17 14:38:11 +0800

    更新配置，使得可解析react

--- webpack.config.js
+++ webpack.config.js
@@ -9,18 +9,25 @@ const config = {
     },
     module: {
         rules: [{
-            test: /\.js$/,
+            test: /\.jsx?$/,
             exclude: /node_modules/,
             use: {
                 loader: "babel-loader",
                 options: {
-                    presets: ['env'],
+                    presets: ['env','react','stage-1'],
                     plugins: ["transform-decorators-legacy", "transform-class-properties"]
                 }
             }
         }]
     },
-    devtool: "inline-source-map"
+    devtool: "source-map",
+    resolve: {
+         // 文件路径优化
+         alias: {
+            react: path.join(__dirname, "src/React"),
+        },
+        extensions: [".js", ".jsx"]
+    }
 }
 
 module.exports = config;
